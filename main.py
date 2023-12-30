52c25468a62f12a336f26daee1216ecb16a41c14
Gyanendu Bikash Boitai <gyanenduboitai2004@gmail.com>
2023-12-30 09:02:11 +0530

    Add more features and comments

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import time
 print("\n\tWelcome to the Grade Sorter App")
 
 def usr_input():
+    # User details
     usr_name = input("\n\tEnter your username: ")
     num_of_grade = int(input("\n\tEnter number of grades that you want to evaluate: "))
     lst_grade = list()
     for i in range(1,num_of_grade+1):
+        # Prohibits the user from entering incorrect grades
         while True:
             grade = float(input("\tEnter grade{} (0-100): ".format(i)))
             if grade >= 0 and grade <= 100:
@@ -21,14 +23,14 @@ def grade_computation(name,grade):
     print("\n\tHello, {} Calculating desired requirements".format(name))
     time.sleep(1)
     print("\n\tYour grades are: {}".format(grade))
-    grade.sort(reverse = True)
+    grade.sort(reverse = True)  # Sort the grades in descending order
     print("\n\tYour grades from highest to lowest: {}".format(grade))
     print("\n\tLowest two grades dropping now....")
     time.sleep(0.6)
     print("\tRemoved grade: {}".format(grade[-1])) 
-    grade.pop()
+    grade.pop()  # Remove the lowest grade
     print("\tRemoved grade: {}".format(grade[-1]))
-    grade.pop()
+    grade.pop()  # Remove the lowest grade
 
     print("\n\tYour remaining grades are: {}".format(grade))
     print("\tYour highest grade is: {}".format(grade[0]))
@@ -36,3 +38,14 @@ def grade_computation(name,grade):
 # Main function
 usr_data = usr_input()
 grade_computation(usr_data[0], usr_data[1])
+print("*"*50)
+
+while True:
+    # Allows the user to use this application as many times as he/she wants
+    condn = input("\n\tDo you want to continue with the grade computationt (y/n): ")
+    if condn.lower() == "y":
+        usr_data = usr_input()
+        grade_computation(usr_data[0], usr_data[1])
+    else:
+        print("\n\tTHANK YOU!!")
+        break
